0e898d6c5ce54fe88c15fd44230359d1cd0f5427
Youssef Azmires <youssef.azmires@yahoo.com>
2025-04-13 14:43:47 -0400

    fefef

---
+++ .history/frontend/src/Components/AddMaintenanceRecordForm_20250413144211.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { JSX } from "react";
+
+export default function Form(): JSX.Element {
+    return (
+        <div>
+            <h1>Form</h1>
+        </div>
+    )
+}
